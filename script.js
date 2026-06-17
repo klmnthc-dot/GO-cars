4bfd5b82c8a2d0de0a3ac662447c98e1ef5b3b2d
klmnthc-dot <klmnthc@gmail.com>
2026-06-17 14:21:22 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// nav shadow effect
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector("nav");
-
-    nav.style.background =
-        window.scrollY > 50
-        ? "rgba(255,255,255,0.15)"
-        : "rgba(255,255,255,0.08)";
-});
