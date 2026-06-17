71e2caa0e553127f79c47ca1ef0adefcddd15326
klmnthc-dot <klmnthc@gmail.com>
2026-06-17 13:49:58 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector("nav");
-
-    if(window.scrollY > 50){
-        nav.style.background = "rgba(0,0,0,0.95)";
-    } else {
-        nav.style.background = "rgba(0,0,0,0.75)";
-    }
-});
