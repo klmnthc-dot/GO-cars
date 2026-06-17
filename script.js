a6e973f8c869b3910406d52a764fb912b7843151
klmnthc-dot <klmnthc@gmail.com>
2026-06-17 13:26:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+
+    if(window.scrollY > 50){
+        nav.style.background = "rgba(0,0,0,0.95)";
+    } else {
+        nav.style.background = "rgba(0,0,0,0.75)";
+    }
+});
